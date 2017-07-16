2b948e5bbd2562576d036623520866d7a8cb85f4
Joseph Siribandan Sriprajittichai <jsripraj@ecegrid-thin1.ecn.purdue.edu>
2017-07-16 14:15:30 -0400

    all test cases successful

--- packing.h
+++ packing.h
@@ -14,16 +14,23 @@ typedef struct Input {
 	int size;
 } Input;
 
+// Loads input from file into an array
 Input loadFromFile(FILE *fp);
 
+// Builds binary tree from information stored in array
 tnode *buildTree(int *pos, tnode **array);
 
+// Frees a binary tree
 void freeTree(tnode *root);
 
+// Gets dimensions in a tree (including dimensions of subtrees)
 void getDimensions(tnode *root);
 
+// Gets coordinates of the leaf nodes in a tree
 void getCoordinates(tnode *root, double xCur, double yCur);
 
+// Writes the leaves of a postorder traversal of a tree to a file
 void writeTreeLeavesPostorder(tnode *root, FILE *fp);
 
+// Gets the largest (highest index) node in a tree
 void getLargestNode(tnode *root, tnode **largest);
--- packing_main.c
+++ packing_main.c
@@ -6,30 +6,43 @@ int main(int argc, char **argv) {
 	// Open the input file
 	FILE *fp = fopen(argv[1], "r");
 	if (fp == NULL) { return EXIT_FAILURE; }
+
 	// Load data from input file into array
 	Input Data = loadFromFile(fp);
+
 	// Close file
 	fclose(fp);
+
 	// Build binary tree from input array
 	tnode *tree = buildTree(&(Data.size), Data.array);
+
 	// Free input array
 	free(Data.array);
+
 	// Calculate dimensions
 	getDimensions(tree);
+
 	// Calculate coordinates
 	getCoordinates(tree, 0, tree -> height);
+
 	// Open the output file
 	fp = fopen(argv[2], "w");
 	if (fp == NULL) { return EXIT_FAILURE; }
+
 	// Print leaf node information to file
 	writeTreeLeavesPostorder(tree, fp);
+
+	// Close the output file
+	fclose(fp);
+
 	// Print screen output
 	printf("\nWidth: %le\nHeight: %le\n", tree -> width, tree -> height);
 	tnode *largest = tree;
 	getLargestNode(tree, &largest);
-	printf("largest label, index = %c, %d\n", largest -> label, largest -> index);
 	printf("X-coordinate: %le\nY-Coordinate: %le\n\n", largest -> x, largest -> y);
+
 	// Free binary tree
 	freeTree(tree);
+
 	return 0;
 }
